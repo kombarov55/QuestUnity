741e866282f4086fd76b595d9e0dae0024721c68
kombarov <kombarov@itopcase.ru>
2022-01-07 16:28:45 +0300

    тост с бекграундом

--- Assets/Other/MatchThreeGame/Assets/Scripts/SceneController.cs
+++ Assets/Other/MatchThreeGame/Assets/Scripts/SceneController.cs
@@ -1,10 +1,23 @@
-﻿using UnityEngine;
+﻿using Other.MatchThreeGame.Assets.Scripts.UI;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Other.MatchThreeGame.Assets.Scripts
 {
     public class SceneController : MonoBehaviour
     {
+
+        [SerializeField] private GameObject toastComponent;
+
+        private ToastBehaviour _toastBehaviour;
+        
+        private void Start()
+        {
+            _toastBehaviour = toastComponent.GetComponent<ToastBehaviour>();
+
+            StartCoroutine(_toastBehaviour.ShowWithFlyAway("Наберите " + Constants.GoalScore + " очков!", 2));
+        }
+
         public void OnBackClicked()
         {
             SceneManager.LoadScene("Scenes/MainMenu");
---
+++ Assets/Other/MatchThreeGame/Assets/Scripts/UI/ToastBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Other.MatchThreeGame.Assets.Scripts.UI
+{
+    public class ToastBehaviour : MonoBehaviour
+    {
+
+        [SerializeField] Text toastText;
+        [SerializeField] GameObject initialTextPosition;
+        [SerializeField] GameObject targetTextPosition;
+
+        public IEnumerator ShowWithFlyAway(string text, int delayInSeconds)
+        {
+            gameObject.SetActive(true);
+            
+            toastText.text = text;
+            toastText.transform.position = initialTextPosition.transform.position;
+            yield return new WaitForSeconds(delayInSeconds);
+            
+            gameObject.transform.positionTo(0.4f, targetTextPosition.transform.position);
+            yield return new WaitForSeconds(0.4f);
+            
+            gameObject.SetActive(false);
+        }
+    }
+}
