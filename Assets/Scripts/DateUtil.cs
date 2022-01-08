d1bb6d95c8fbc2e74e9e7fe4875fc94bc0822601
kombarov <kombarov@itopcase.ru>
2022-01-08 22:11:59 +0300

    dateutil, prefs

---
+++ Assets/Scripts/DateUtil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace DefaultNamespace
+{
+    public static class DateUtil
+    {
+        private static readonly string Pattern = "yyyy'-'MM'-'dd'T'HH':'mm':'ss";
+        
+        public static string DateToString(DateTime dateTime)
+        {
+            return dateTime.ToString(Pattern);
+        }
+
+        public static DateTime StringToDate(string str)
+        {
+            return DateTime.ParseExact(str, Pattern, CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Assets/Scripts/Prefs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Prefs
+    {
+        private static readonly string LifesKey = "ThreeInARowLifes";
+        private static readonly string LastLifeCountdownUpdateKey = "LastLifeCountdownUpdateKey";
+        
+        public static int GetLifes()
+        {
+            return PlayerPrefs.GetInt(LifesKey);
+        }
+
+        public static void SetLifes(int lifes)
+        {
+            PlayerPrefs.SetInt(LifesKey, lifes);
+        }
+
+        public static DateTime GetLastLifeCountdownUpdate()
+        {
+            return DateUtil.StringToDate(PlayerPrefs.GetString(LastLifeCountdownUpdateKey));
+        }
+
+        public static void SetLastLifeCountdownUpdate(DateTime dateTime)
+        {
+            PlayerPrefs.SetString(LastLifeCountdownUpdateKey, DateUtil.DateToString(dateTime));
+        }
+        
+    }
+}
