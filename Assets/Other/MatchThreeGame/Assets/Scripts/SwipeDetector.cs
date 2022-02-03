c1d1b8c555ed8a8def3286d24c2c3ee929f9d565
kombarov <kombarov@itopcase.ru>
2022-02-04 00:51:46 +0300

    вроде починил свайпы по диагонали

--- Assets/Other/MatchThreeGame/Assets/Scripts/SwipeDetector.cs
+++ Assets/Other/MatchThreeGame/Assets/Scripts/SwipeDetector.cs
@@ -20,61 +20,68 @@ namespace Other.MatchThreeGame.Assets.Scripts
 
         private void Update () {
             if (Input.GetMouseButtonDown(0)) {
-                this.fingerDown = Input.mousePosition;
-                this.fingerUp = Input.mousePosition;
-                this.fingerDownTime = DateTime.Now;
+                fingerDown = Input.mousePosition;
+                fingerUp = Input.mousePosition;
+                fingerDownTime = DateTime.Now;
             }
             if (Input.GetMouseButtonUp(0)) {
-                this.fingerDown = Input.mousePosition;
-                this.fingerUpTime = DateTime.Now;
-                this.CheckSwipe();
+                fingerDown = Input.mousePosition;
+                fingerUpTime = DateTime.Now;
+                CheckSwipe();
             }
             foreach (Touch touch in Input.touches) {
                 if (touch.phase == TouchPhase.Began) {
-                    this.fingerDown = touch.position;
-                    this.fingerUp = touch.position;
-                    this.fingerDownTime = DateTime.Now;
+                    fingerDown = touch.position;
+                    fingerUp = touch.position;
+                    fingerDownTime = DateTime.Now;
                 }
                 if (touch.phase == TouchPhase.Ended) {
-                    this.fingerDown = touch.position;
-                    this.fingerUpTime = DateTime.Now;
-                    this.CheckSwipe();
+                    fingerDown = touch.position;
+                    fingerUpTime = DateTime.Now;
+                    CheckSwipe();
                 }
             }
         }
 
         private void CheckSwipe() {
-            float duration = (float)this.fingerUpTime.Subtract(this.fingerDownTime).TotalSeconds;
-            if (duration > this.timeThreshold) return;
+            float duration = (float)fingerUpTime.Subtract(fingerDownTime).TotalSeconds;
+            if (duration > timeThreshold) return;
 
-            float deltaX = this.fingerDown.x - this.fingerUp.x;
-            if (Mathf.Abs(deltaX) > this.swipeThreshold) {
-                if (deltaX > 0) {
-                    OnSwipe.Emit(new Tuple<Vector2, Direction>(fingerUp, Direction.Right));
-                    
-                    // this.OnSwipeRight.Invoke();
-                    //Debug.Log("right");
-                } else if (deltaX < 0) {
-                    OnSwipe.Emit(new Tuple<Vector2, Direction>(fingerUp, Direction.Left));
-                    // this.OnSwipeLeft.Invoke();
-                    //Debug.Log("left");
-                }
-            }
-
+            float deltaX = fingerDown.x - fingerUp.x;
             float deltaY = fingerDown.y - fingerUp.y;
-            if (Mathf.Abs(deltaY) > this.swipeThreshold) {
-                if (deltaY > 0) {
-                    // this.OnSwipeUp.Invoke();
-                    OnSwipe.Emit(new Tuple<Vector2, Direction>(fingerUp, Direction.Up));
-                    //Debug.Log("up");
-                } else if (deltaY < 0) {
-                    OnSwipe.Emit(new Tuple<Vector2, Direction>(fingerUp, Direction.Down));
-                    // this.OnSwipeDown.Invoke();
-                    //Debug.Log("down");
-                }
-            }
 
-            this.fingerUp = this.fingerDown;
+            if (Math.Abs(deltaX) > Math.Abs(deltaY))
+            {
+                if (Mathf.Abs(deltaX) > swipeThreshold) {
+                    if (deltaX > 0) {
+                        OnSwipe.Emit(new Tuple<Vector2, Direction>(fingerUp, Direction.Right));
+                    
+                        // OnSwipeRight.Invoke();
+                        //Debug.Log("right");
+                    } else if (deltaX < 0) {
+                        OnSwipe.Emit(new Tuple<Vector2, Direction>(fingerUp, Direction.Left));
+                        // OnSwipeLeft.Invoke();
+                        //Debug.Log("left");
+                    }
+                }                
+            }
+            else
+            {
+                if (Mathf.Abs(deltaY) > swipeThreshold) {
+                    if (deltaY > 0) {
+                        // OnSwipeUp.Invoke();
+                        OnSwipe.Emit(new Tuple<Vector2, Direction>(fingerUp, Direction.Up));
+                        //Debug.Log("up");
+                    } else if (deltaY < 0) {
+                        OnSwipe.Emit(new Tuple<Vector2, Direction>(fingerUp, Direction.Down));
+                        // OnSwipeDown.Invoke();
+                        //Debug.Log("down");
+                    }
+                }                
+            }
+            
+
+            fingerUp = fingerDown;
         }
     }
 }
